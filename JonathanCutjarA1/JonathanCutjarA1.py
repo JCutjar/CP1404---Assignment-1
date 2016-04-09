a4179cfb535e885e706c885aed883142289cb404
JCutjar <jonathan.cutjar@my.jcu.edu.au>
2016-04-09 20:13:09 +1000

    Final Stages

--- JonathanCutjarA1/JonathanCutjarA1.py
+++ JonathanCutjarA1/JonathanCutjarA1.py
@@ -1,74 +1,132 @@
-
 import csv as csv
-
-
-
 
 
 def main():
     print("Items for Hire - by Jonathan Cutjar")
+
     is_running = True
     rental_list = []
-    with open("rental_list.csv", newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter = ',', quotechar='\n')
+    with open("items.csv", newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='\n')
         for row in reader:
             rental_list.append(row)
+    print(str(len(rental_list)) + " items loaded from items.csv")
     while is_running:
         print("Menu:\n(L)ist all items\n(H)ire an item\n(R)eturn an item\n(A)dd new item to stock\n(Q)uit")
         menu_choice = input()
         menu_choice = menu_choice.upper()
         if menu_choice == "L":
-            print("choice L")
             choice_list_all_items(rental_list)
-
         elif menu_choice == "H":
-            print("choice H")
             choice_hire_an_item(rental_list)
-
         elif menu_choice == "R":
-            print("choice R")
             choice_return_an_item(rental_list)
-
         elif menu_choice == "A":
-            print("choice A")
             choice_add_new_item(rental_list)
-
         elif menu_choice == "Q":
-            print("choice Q")
-            choice_quit()
-
+            is_running = choice_quit(rental_list)
         else:
-            print("incorrect input")
+            print("Incorrect input.")
+
 
 def choice_list_all_items(rental_list):
-    print("Any item marked with an astrix (*) next to it's price, is currently hired out and unavailable for hire.")
+    print("All items on file (* indicates item is currently out):")
     for row in rental_list:
         if row[3] == "out":
             print("{:20} = ${:8.2f} *".format(row[0], float(row[2])))
         else:
             print("{:20} = ${:8.2f}".format(row[0], float(row[2])))
 
+
 def choice_hire_an_item(rental_list):
-    print("Hello H")
+    index_tracker = 0
+    nothing_to_hire_boolean = True
     for row in rental_list:
         if row[3] == "in":
-            print("{:20} = ${:8.2f}".format(row[0], float(row[2])))
-        else:
-            print("There are currently no items available for hire.")
+            nothing_to_hire_boolean = False
+            print("{:20} = ${:8.2f}".format(str(index_tracker) + " - " + row[0], float(row[2])))
+        index_tracker += 1
+    if nothing_to_hire_boolean:
+        print("No items available for hire. Returning to main menu")
+        return
+    item_to_hire = error_check_input(rental_list)
+    if item_to_hire.upper() == "Q":
+        return
+    else:
+        rental_list[int(item_to_hire)][3] = "out"
+
 
 def choice_return_an_item(rental_list):
-    print("Hello R")
+    index_tracker = 0
+    nothing_to_return_boolean = True
     for row in rental_list:
         if row[3] == "out":
-            print("{:20} = ${:8.2f}".format(row[0], float(row[2])))
-        else:
-            print("There are currently no items available to return.")
+            nothing_to_return_boolean = False
+            print("{:20} = ${:8.2f}".format(str(index_tracker) + " - " + row[0], float(row[2])))
+        index_tracker += 1
+    if nothing_to_return_boolean:
+        print("No items available for return. Returning to main menu")
+        return
+    item_to_return = error_check_input(rental_list)
+    if item_to_return.upper() == "Q":
+        return
+    else:
+        rental_list[int(item_to_return)][3] = "in"
+
+
+def error_check_input(rental_list):
+    valid_input_boolean = False
+    input_to_be_checked = ""
+    while not valid_input_boolean:
+        input_to_be_checked = input("Which item would you like to hire? or Q to Quit")
+        try:
+            if input_to_be_checked.upper() == "Q":
+                valid_input_boolean = True
+            elif int(input_to_be_checked) <= len(rental_list) - 1:
+                valid_input_boolean = True
+            else:
+                print("Invalid input.")
+        except ValueError:
+            print("Invalid input.")
+    return input_to_be_checked
+
 
 def choice_add_new_item(rental_list):
-    print("Hello A")
+    item_name = input("Item name: ")
+    while item_name == "":
+        print("Input cannot be blank")
+        item_name = input("Item name: ")
+    item_description = input("Description: ")
+    while item_description == "":
+        print("Input cannon be blank")
+        item_description = input("Description: ")
+    item_price_per_day = 0.0
+    price_is_valid_boolean = False
+    while not price_is_valid_boolean:
+        item_price_per_day = input("Price per day: $")
+        try:
+            if item_price_per_day == "":
+                print("Input cannon be blank")
+            elif float(item_price_per_day) < 0.0:
+                print("Price must be > or = $0")
+                print("Invalid input; enter a valid number")
+            else:
+                price_is_valid_boolean = True
+        except ValueError:
+            print("Invalid input; enter a valid number")
+    new_item = [item_name, item_description, item_price_per_day, "in"]
+    print(new_item[0] + " (" + new_item[1] + "), $" + new_item[2] + " now available for hire.")
+    rental_list.append(new_item)
 
-def choice_quit():
-    print("Hello Q")
-    is_running = False
+
+def choice_quit(rental_list):
+    program_boolean = False
+    with open('items.csv', 'w', newline='\n') as file:
+        writer = csv.writer(file, delimiter=',')
+        writer.writerows(rental_list)
+    print(str(len(rental_list)) + " items saved to items.csv")
+    print("Have a nice day! :)")
+    return program_boolean
+
 
 main()
